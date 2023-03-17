ab77a062e6131734b9ea4d06924a5b2f2437551d
vanara.leng <vanara.leng@asiacell.com>
2023-03-17 12:57:40 -0500

    update routes for auth module

--- EventClientApp/src/app/auth/auth.module.ts
+++ EventClientApp/src/app/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -11,7 +12,11 @@ import { SignupComponent } from './signup/signup.component';
     SignupComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+     RouterModule.forChild([
+      { path: 'login', component: LoginComponent },
+      { path: 'signup', component: SignupComponent }
+     ])
   ]
 })
 export class AuthModule { }
