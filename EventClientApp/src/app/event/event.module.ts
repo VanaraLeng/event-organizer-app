ab8bf962272fe8dae295801e1f14bed5fbab5099
Phyo <phyotheingi92@gmail.com>
2023-03-19 05:08:52 -0500

    create event

--- EventClientApp/src/app/event/event.module.ts
+++ EventClientApp/src/app/event/event.module.ts
@@ -5,17 +5,26 @@ import { CreateEventComponent } from './create-event/create-event.component';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../material.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { UpdateEventComponent } from './update-event/update-event.component';
+import { ViewEventComponent } from './view-event/view-event.component';
+import { ListEventComponent } from './list-event/list-event.component';
 
 @NgModule({
   declarations: [
-    CreateEventComponent
+    CreateEventComponent,
+    UpdateEventComponent,
+    ViewEventComponent,
+    ListEventComponent
   ],
   imports: [
     CommonModule,
     MaterialModule,
     ReactiveFormsModule,
     RouterModule.forChild([
-      { path: 'create', component: CreateEventComponent }
+      { path: 'create', component: CreateEventComponent },
+      { path: 'update', component: UpdateEventComponent },
+      { path: 'list', component: ListEventComponent },
+      { path: 'view', component: ViewEventComponent },
     ])
   ]
 })
