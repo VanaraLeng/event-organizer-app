26e06335418aaccb50b2d5292a8fef98a612db63
vanara.leng <vanara.leng@asiacell.com>
2023-03-20 18:37:26 -0500

    Update user profile for other user

--- EventClientApp/src/app/user/user-profile/user-profile.component.ts
+++ EventClientApp/src/app/user/user-profile/user-profile.component.ts
@@ -15,58 +15,98 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class UserProfileComponent {
   router = inject(Router);
   activatedRoute = inject(ActivatedRoute);
-  userSevice = inject(UserService);
+  userService = inject(UserService);
 
   form = inject(FormBuilder).group({
     firstname: [''],
     lastname: [''],
     email: ['', [Validators.required, Validators.email]],
     bio: [''],
+    address: [''],
+    latitude: [''],
+    longitude: [''],
   })
 
   user!: IUser;
   photoUrl!: string;
   notification = inject(MatSnackBar);
 
+  isMe = false;
+
   constructor() {
+
     this.activatedRoute.paramMap
       .pipe(
         map(params => params.get('user_id') as string),
-        mergeMap(user_id => this.userSevice.getUser(user_id))
+        mergeMap(user_id => this.userService.getUser(user_id))
       )
       .subscribe({
         next: response => {
+
+          
+
           this.user = response.data.user as IUser;
           if (this.user.photo) this.photoUrl = environment.PHOTO_BASE_URL + this.user.photo.filename;
           else this.photoUrl = "";
+
+          const myId = this.userService.state$.value._id;
+          this.isMe = this.user._id === myId;
 
           this.form.setValue({
             firstname: this.user.firstName,
             lastname: this.user.lastName,
             email: this.user.email,
-            bio: this.user.bio
+            bio: this.user.bio,
+            address: '',
+            latitude:  String(this.user.location[0]),
+            longitude: String(this.user.location[1])
           })
         }
       });
   }
 
+  getGeoLocation() {
+    console.log('Getting address: ', this.form.value.address);
+    this.userService.getLocation(this.form.value.address ? this.form.value.address : "52557").subscribe({
+      next: (res) => {
+        console.log("res ==>", res);
+
+        if (res.status == 'OK') {
+          this.form.setValue({
+            firstname: this.form.value.firstname ? this.form.value.firstname : '',
+            lastname: this.form.value.lastname ? this.form.value.lastname : '',
+            email: this.form.value.email ? this.form.value.email : '',
+            bio: this.form.value.bio ? this.form.value.bio : '',
+            address: this.form.value.address ? this.form.value.address : '',
+            latitude: res.results[0].geometry.location.lat,
+            longitude: res.results[0].geometry.location.lng,
+          })
+        } else {
+          this.notification.open(res.message, 'Dismiss', { duration: 3 * 1000 })
+        }
+      },
+      error: (e) => {
+        this.notification.open(e.message, 'Dismiss', { duration: 3 * 1000 })
+      }
+    })
+  }
+
   update() {
     if (this.form.dirty) {
+      const formValue = this.form.value;
       const updateUser = {
-        firstName: this.form.value.firstname as string,
-        lastName: this.form.value.lastname as string,
-        email: this.form.value.email as string,
-        bio: this.form.value.bio as string,
+        firstName: formValue.firstname as string,
+        lastName: formValue.lastname as string,
+        email: formValue.email as string,
+        bio: formValue.bio as string,
+        location: [ Number(formValue.latitude), Number(formValue.longitude)]
       };
-      this.userSevice.updateUser(this.user._id, updateUser).subscribe({
+      this.userService.updateUser(this.user._id, updateUser).subscribe({
         next: (res) => {
           console.log("res ==>", res);
 
           if (res.success === true) {
-            this.notification.open("Successfully update profile!", "", {
-              horizontalPosition: 'end',
-              verticalPosition: 'top', duration: 3 * 1000
-            })
+            this.notification.open("Successfully update profile!", "", { duration: 3 * 1000 })
           } else {
             this.notification.open(res.message, 'Dismiss', { duration: 3 * 1000 })
           }
