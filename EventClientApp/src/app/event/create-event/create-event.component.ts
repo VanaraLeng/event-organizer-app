46ee28628492c18a2ec10b593a6a0111f02871dc
Phyo <phyotheingi92@gmail.com>
2023-03-20 14:56:37 -0500

    Geo for Create Evernt

--- EventClientApp/src/app/event/create-event/create-event.component.ts
+++ EventClientApp/src/app/event/create-event/create-event.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { EventService } from '../event.service';
+import { UserService } from '../../user.service';
 
 @Component({
   selector: 'app-create-event',
@@ -20,6 +21,7 @@ export class CreateEventComponent {
   secondFormGroup = inject(FormBuilder).group({
     latitude: [''],
     longitude: [''],
+    address: ['52557'],
     seatLimit: ['', Validators.max(100)]
   });
   thirdFormGroup = inject(FormBuilder).group({
@@ -27,6 +29,7 @@ export class CreateEventComponent {
   });
 
   eventService = inject(EventService);
+  userService = inject(UserService);
   notification = inject(MatSnackBar);
 
   constructor(private router: Router) { }
@@ -51,6 +54,29 @@ export class CreateEventComponent {
             verticalPosition: 'top', duration: 3 * 1000
           })
           this.router.navigate(['', 'event', 'me']);
+        } else {
+          this.notification.open(res.message, 'Dismiss', { duration: 3 * 1000 })
+        }
+      },
+      error: (e) => {
+        this.notification.open(e.message, 'Dismiss', { duration: 3 * 1000 })
+      }
+    })
+  }
+
+  getGeoLocation() {
+    console.log('Getting address: ', this.secondFormGroup.value.address);
+    this.userService.getLocation(this.secondFormGroup.value.address ? this.secondFormGroup.value.address : "52557").subscribe({
+      next: (res) => {
+        console.log("res ==>", res);
+
+        if (res.status == 'OK') {
+          this.secondFormGroup.setValue({
+            latitude: res.results[0].geometry.location.lat,
+            longitude: res.results[0].geometry.location.lng,
+            seatLimit: this.secondFormGroup.value.seatLimit ? this.secondFormGroup.value.seatLimit : '',
+            address: this.secondFormGroup.value.address ? this.secondFormGroup.value.address : '',
+          })
         } else {
           this.notification.open(res.message, 'Dismiss', { duration: 3 * 1000 })
         }
