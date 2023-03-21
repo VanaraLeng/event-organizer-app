6f8aca25034016f4b03322f0acc9a2ac5aa42b9c
vanara.leng <vanara.leng@asiacell.com>
2023-03-21 18:30:42 -0500

    Display message for different errors

--- EventClientApp/src/app/request.interceptor.ts
+++ EventClientApp/src/app/request.interceptor.ts
@@ -3,9 +3,10 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 import { environment } from '../environments/environment'
 import { UserService } from './user.service';
 
@@ -23,7 +24,40 @@ export class RequestInterceptor implements HttpInterceptor {
       });
     }
 
+    return next.handle(request).pipe(
+      catchError((error) => {
+        if (error instanceof HttpErrorResponse) {
+          let newError!: Error
+          switch (error.status) {
+            case 0:
+              newError = new Error("No internet connection. Please connect internet and try again.");
+              break;
 
-    return next.handle(request);
+            case 500:
+              newError = new Error("There is technical problem. Please try again later");
+              break;
+
+            case 400:
+              newError = new Error("Your request is incorrect.")
+              break;
+
+            case 401:
+            case 403:
+              newError = new Error("You don't have access to this URL.")
+              break;
+
+            case 404:
+              newError = new Error("URL not found.")
+              break;
+
+            default:
+              newError = new Error("Unknow error. Please try again later.");
+          }
+          return throwError(() => newError)
+        } else {
+          return throwError(() => error);
+        }
+      })
+    );
   }
 }
