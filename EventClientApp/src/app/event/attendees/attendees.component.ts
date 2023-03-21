8150d0b4be85d154d344f2052741b19c06af14ef
vanara.leng <vanara.leng@asiacell.com>
2023-03-21 18:32:47 -0500

    Add profile image for attendee list

--- EventClientApp/src/app/event/attendees/attendees.component.ts
+++ EventClientApp/src/app/event/attendees/attendees.component.ts
@@ -1,5 +1,7 @@
+import { Location } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { IUser } from 'src/app/IUser.interface';
+import { environment } from 'src/environments/environment';
 import { EventService } from '../event.service';
 
 @Component({
@@ -9,21 +11,27 @@ import { EventService } from '../event.service';
 })
 export class AttendeesComponent {
   attendees?: IUser[] = inject(EventService).attendees;
-
-  displayedColumns: string[] = ['order', 'firstName', 'lastName', 'email'];
+  photoUrl = environment.PHOTO_BASE_URL;
+  displayedColumns: string[] = [ 'order', 'photo', 'firstName', 'lastName', 'email'];
   dataSource: object[] = [];
 
-  constructor() {
+  constructor(private _location: Location) {
     let order = 1;
     this.attendees?.forEach(attendee => {
       const data = {
         order: order,
         firstName: attendee.firstName,
         lastName: attendee.lastName,
-        email: attendee.email
+        email: attendee.email,
+        photo: attendee.photo
       }
       this.dataSource.push(data);
       order++;
     });
+    console.log(this.attendees);
+  }
+  
+  goback() {
+    this._location.back();
   }
 }
